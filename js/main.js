67be66f012dfeca603691cc4a91e5278c8d4679e
estudiografico1236 <arqjackeline1236@gmail.com>
2023-08-12 21:52:36 -0300

    mi primer commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const menuboton = document.querySelector('#menu_boton')
+menuboton.addEventListener('click',function(){document.body.classList.toggle('mobile_menu_active')
+})
+
+const modooscuro = document.querySelector('#modo_oscuro_btn')
+modooscuro.addEventListener('click',function(){document.body.classList.toggle('modo_oscuro')
+})
+
